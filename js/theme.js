741b6562bae49eaa575a766272f734787ffc5510
iccanobif <iccanobif@fake.mail>
2020-04-22 22:48:58 +0200

    miscellane

--- js/theme.js
+++ js/theme.js
@@ -6,7 +6,7 @@
  function prealoader () {
    if ($('#preloader_1').length) {
      $('#preloader_1').fadeOut(); // will first fade out the loading animation
-     $('#loader-wrapper').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    //  $('#loader-wrapper').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
      $('body').delay(350).css({'overflow':'visible'});
   };
  }
